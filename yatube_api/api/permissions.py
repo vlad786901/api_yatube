eb4a0421dc07412dfef80999e49b7989a129db30
Владислав Лутков <vlad786901@mail.ru>
2023-09-07 15:15:37 +0400

    fix

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -6,4 +6,3 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
                 or obj.author == request.user)
-    
